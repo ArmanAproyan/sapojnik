f074b520232661eb30b3b6c19f9892374c3f800f
Arman <aaproyan@bk.ru>
2024-10-16 17:02:44 +0400

    add-next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    reactStrictMode: true, // Включить строгий режим React
+    swcMinify: true, // Использовать SWC для минимизации
+    images: {
+      // Если вы используете изображения из внешних источников, добавьте их здесь
+      domains: ['example.com'], // Замените на ваши домены
+    },
+    env: {
+      // Если у вас есть переменные окружения, можете указать их здесь
+      API_URL: process.env.API_URL, // Пример переменной окружения
+    },
 };
 
 export default nextConfig;
