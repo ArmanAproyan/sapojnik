f5f262b38a68829c0790fb3ecc1719ae5f1369aa
Arman <aaproyan@bk.ru>
2024-10-16 17:19:37 +0400

    chnage axios fetch

--- app/components/complex/reviewsPerson/ReviewsPerson.tsx
+++ app/components/complex/reviewsPerson/ReviewsPerson.tsx
@@ -1,64 +1,56 @@
 'use client';
 import style from './style.module.scss';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import { IreviewDataProps } from '@/app/types/types';
 import { RiUser3Fill } from "react-icons/ri";
 import SkeletonReview from '../../simple/reviewSkeleton/ReviewSkeleton';
 
-
-
-
-const ReviewsPerson:React.FC<IreviewDataProps> = ({reviewData,setReviewData}) => {
-
-    const [loading,setLoading] = useState<boolean>(true);
+const ReviewsPerson: React.FC<IreviewDataProps> = ({ reviewData, setReviewData }) => {
+    const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
         const getData = async () => {
-            console.log("Выполняется запрос к базе данных для получения отзывов.");
-
             try {
-                axios.get('/api/reviews')
-                    .then((response) => {
-                         const data: any = response.data
-                        setReviewData(data)
-                        setLoading(false)
-                    })
+                const response = await fetch('/api/reviews');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data: any = await response.json();
+                setReviewData(data);
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
             }
-            catch (error) {
-                console.log(error)
-            }
-        }
-        getData()
-    }, [])
+        };
+        getData();
+    }, []);
 
     return (
-
         <div className={style.reviews_container}>
             <div className={style.review_text}>
-                <h1>Наши Отзивы</h1>
+                <h1>Наши Отзывы</h1>
             </div>
-        <div className={style.reviews}>
-            <div className={style.review_block}>
-                {loading && <SkeletonReview/>}
-                {reviewData && reviewData.map((val) => {
-                    return (
-                        <div key={val.id} className={style.review}>
-                            <div className={style.user_info}>
-                                <div className={style.user_image}><RiUser3Fill/></div>
-                                <div className={style.user_email}>{val.email}</div>
-                                <div className={style.time}>{val.sendTime}</div>
+            <div className={style.reviews}>
+                <div className={style.review_block}>
+                    {loading && <SkeletonReview />}
+                    {reviewData && reviewData.map((val) => {
+                        return (
+                            <div key={val.id} className={style.review}>
+                                <div className={style.user_info}>
+                                    <div className={style.user_image}><RiUser3Fill /></div>
+                                    <div className={style.user_email}>{val.email}</div>
+                                    <div className={style.time}>{val.sendTime}</div>
+                                </div>
+                                <div className={style.user_review}>
+                                    <p>{val.review}</p>
+                                </div>
                             </div>
-                            <div className={style.user_review}>
-                                <p>{val.review}</p>
-                            </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
         </div>
     )
 }
 
-export default ReviewsPerson
+export default ReviewsPerson;
